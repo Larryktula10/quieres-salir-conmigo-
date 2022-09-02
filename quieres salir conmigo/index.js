f721e04b0d905be74d74e3946c58f22663fa11b8
Larryktula10 <112680973+Larryktula10@users.noreply.github.com>
2022-09-02 00:20:28 -0500

    Add files via upload

---
+++ quieres salir conmigo/index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const documentReady = () => {
+  const heroTitle = document.querySelector('.hero__title');
+  const heroButtonSi = document.getElementById('heroButtonSi');
+  const heroButtonNo = document.querySelector('#heroButtonNo');
+
+  const nowZomosNobios = () => {
+    alert('💞 Sabia que dirias que si 💞');
+    alert('😱Besitos en tu colita😱');
+    location.href = 'https://www.youtube.com/watch?v=GuZzuQvv7uc';
+  };
+
+  const evitarKCRompanMyBobo = () => {
+    heroButtonNo.style.position = 'absolute';
+    heroButtonNo.style.top = (Math.random() * window.innerHeight) + 'px';
+    heroButtonNo.style.left = (Math.random() * window.innerWidth) + 'px';
+  };
+
+  const partner = prompt('🥰 Hola como estas? 🥰');
+  
+
+  heroButtonSi.addEventListener('click', nowZomosNobios);
+  heroButtonNo.addEventListener('mouseover', evitarKCRompanMyBobo);
+};
+
+document.addEventListener('DOMContentLoaded', documentReady);
